773721e4d51bc208ac56064fa88899218a89a951
AFlak <AndrewMFlak@gmail.com>
2018-12-04 12:02:15 -0500

    functional scrape of data

--- index.py
+++ index.py
@@ -28,142 +28,102 @@ nonLive = []
 records = []
 Total = ''
 Finished = False
-end = 5
+end = 44000
 page = 1
-url = 'http://openleis.com/legal_entities/search/page/' + str(page)
+itemCount = 0
+# liveItems = ''
+# not_liveItems = ''
+# liveItemCount = 0
+# notLiveItemCount = 0
+df = pd.DataFrame(columns=['EntityStatus','Country', 'LEI' ,'Name', 'RegistrationStatus', 'RecordCount'])
 #======================================================>
+while page <= end:
+    try:
+        url = 'http://openleis.com/legal_entities/search/page/' + str(page)
 # .read() extracts all data from url provided
-# context=ctx to bypass SSL ERROR
 #=========WHERE TO PUT THIS SHIT==================================>
-# while page <= end:
-#         try:    
-html = urllib.request.urlopen(url,context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
-paginationContainer = soup.find('div', {'class':'pagination'})
-pageTurner = 'http://openleis.com'
-pathFiller = '/legal_entities/search/page/'
+# context is equal to ctx. Is used to bypass SSL ERROR
+        html = urllib.request.urlopen(url,context=ctx).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        paginationContainer = soup.find('div', {'class':'pagination'})
+        pageTurner = 'http://openleis.com'
+        pathFiller = '/legal_entities/search/page/'
 
 #================================================================>
 
-
-
-#====================ParentDiv parse============================>
-parentDiv = soup.find('section', {'class':'results row'})
+#==================== ParentDiv parse ============================>
+        parentDiv = soup.find('section', {'class':'results row'})
 #===============================================================>
 
 
 #====================TotalValue parse============================>
-totalContainer = parentDiv.find_all('h1')
-for line in totalContainer:
-        # print(type(line))
-        Total=line.get_text()
-        print(Total)
+        #SHIT DONT WORK
+        # totalContainer = parentDiv.find_all('h1')
+        # for line in totalContainer:
+        #         # print(type(line))
+        #         Total=line.get_text()
+        #         print(Total)
 #===============================================================>
 
 
 #====================itemInfo parse==============================>
-liveItems = ''
-not_liveItems = ''
-liveItemCount = 0
-notLiveItemCount = 0
-df = []
-contentContainer = parentDiv.find('ul', {'class':'results-list with-flags'})
-itemContainer = contentContainer('li')
-# print(itemContainer)
-liveItems = contentContainer.find_all('li', {'class':'live'})
-# print(liveItems)
-not_liveItems = contentContainer.find_all('li',{'class':'not_live'})
+        #soup row containing all content to scrape
+
+        contentContainer = parentDiv.find('ul', {'class':'results-list with-flags'})
+        itemContainer = contentContainer('li')
+
+        #soup div find to produce number of pages to iterate
+
+        endChild = parentDiv.find('a', {'class':'next_page'})
+        EndNode = endChild.find_previous_sibling()
+        End = int(EndNode.text)
+        print('end of scrape value: ',End)
+
+        # <a href="/legal_entities/search/page/43997/page/1">43997</a>
+        # <a class="next_page" rel="next" href="/legal_entities/search/page/2/page/1">Next →</a>
 
 
-#=============================Item Iterate==============================>
-for item in itemContainer:
-# for liveItem in liveItems:
-        print('<=====================New Record=====================>')
-        statusContainer = item['class']
-        status = str(statusContainer[0])
-        print('1. ',status)
-        container = item.find('a' , attrs = {'class':'flag'})
-        country = container['href']
-        country = country[-2:]
-        print('2. ',country)
-        nameContainer = item.find('a' , attrs = {'class':'label'})
-        name = nameContainer.text
-        name = name.replace('"','')
-        name = name.strip()
-        print('3. ',name)
-        statusContainer = item.find('span', attrs = {'title':'Lei Registration Status'})
-        print('4. ',statusContainer.text)
-        entityStatusContainer = item.find('span', attrs = {'title':'Entity Status'})
-        entityStatus = entityStatusContainer.text
-        print('5. ',entityStatus)
-        noteContainer = item.find('span', attrs = {'class':'note'})
-        LEI = noteContainer.a.text
-        print('6. ',LEI)
-        liveItemCount = liveItemCount + 1
-        print('7. ',liveItemCount)
-        print('<===================Record Complete===================>')
-        print('')
-        print('')
-#=======================================================================>
+#============================= Scrape Iterate Item ==============================>
+        for item in itemContainer:
+                # print('<=====================New Record=====================>')
+                statusContainer = item['class']
+                status = str(statusContainer[0])
+                # print('1. ',status)
+                container = item.find('a' , attrs = {'class':'flag'})
+                country = container['href']
+                country = country[-2:]
+                # print('2. ',country)
+                nameContainer = item.find('a' , attrs = {'class':'label'})
+                name = nameContainer.text
+                name = name.replace('"','')
+                name = name.strip()
+                # print('3. ',name)
+                statusContainer = item.find('span', attrs = {'title':'Lei Registration Status'})
+                registrationStatus = str(statusContainer.text)
+                print('4. ',registrationStatus)
+                entityStatusContainer = item.find('span', attrs = {'title':'Entity Status'})
+                entityStatus = entityStatusContainer.text
+                # print('5. ',entityStatus)
+                noteContainer = item.find('span', attrs = {'class':'note'})
+                LEI = noteContainer.a.text
+                # print('6. ',LEI)
+                itemCount = itemCount + 1
+                # print('7. ',liveItemCount)
+                # print('<===================Record Complete===================>')
+                # print('')
+                # print('')
+                df = df.append({'EntityStatus':entityStatus,'Country':country, 'LEI':LEI ,'Name':name, 'RegistrationStatus':registrationStatus, 'RecordCount':itemCount},ignore_index=True)
+        page = page + 1
+        print(df)
+        print('page: ', page)
+        if page == End:
+            print('Data.gov scrape complete')
+            exit()
+    except:
+        print("fail or complete check count to verify complete.  should be good.")
+        exit()
+#======================= Scrape Iterate Item End ===============================>
 #
-#
-#
-# for child in parentDiv.descendants:
-#     print(child)
-
-# totalDiv = soup.find('section', {'class':'results row'})
-# totalContainer = soup.find('h1')
-
-# 
-    # while Total == '':
-    #     totCondition = child.find('h1')
-    #     print(totCondition.contents)
-    #     Total = 1
-
-        # total = totCondition.find_next_sibling("em")
-        # print(total)
-    # child.find('li', {'class':'results-list with-flags'})
-# r# for child in ParentDiv.children:
-#     print(child)
-
-# for child in children:
-#     print(child)
-
-
-# for node in container:
-#     print(node)
-    # try:
-    #     totalRecord = container.find('h1')
-    #     print(node)
-    # except:
-    #     print('h1 scrape fail')
-
-    # try:
-    #     leiRecords = container.find_all('li')
-    # except:
-    #     print('li scrape fail')
-    
-
-# for singleRecord in leiRecords:
-#     print('sr: ',leiRecords)
-
-
-# live = soup.find_all('li', {'class':'live'})
-# nonLive = soup.find_all('li', {'class':'not_live'})
-
-#===================Total LEI count================================>
-# for tr in totalRecord:
-#     print('tr: ',tr)
-
-# Total = soup.find('em').text
-# print('Total: ',Total)
-
-#==================================================================>
-
-# count = 0
-# for em in ems:
-#     print(count + 1,':', em)
-
 
 #sanity check
 # url = 'https://www.financialresearch.gov/data/legal-entity-identifier/'
@@ -177,14 +137,6 @@ for item in itemContainer:
 # for p in pTags:
 #     print('')
 # spanTags = soup('span')
-#====================basic workflow model========================>
-# while Finished == False:
-#     try:
-#         scrape()
-#     except:
-#         print('LEI scrape complete')
-
-#================================================================>
 
 # testTotal = soup.find('p', {'class':'map-hed'})
 # print(testTotal.get_text())
