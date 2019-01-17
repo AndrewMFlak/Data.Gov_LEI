060018465a06f45797ebc9a65e5dbdbc2a103c93
AFlak <AndrewMFlak@gmail.com>
2019-01-16 21:19:29 -0500

    functional push

--- scripts/alt.py
+++ scripts/alt.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from bs4 import BeautifulSoup
 import time
 import datetime
 import io
@@ -44,8 +45,8 @@ col = db["leisJSON"]
 
 # count variables not used
 ct = 0
-# workflow page variables
-page = 1
+# workflow page variable
+lastPage = 0
 End = 0
 #FUNCTIONS
 
@@ -54,14 +55,14 @@ def getEnd(page):
     html = urllib.request.urlopen(url,context=ctx).read()
     soup = BeautifulSoup(html, 'html.parser')
     parentDiv = soup.find('section', {'class':'results row'})
-    contentContainer = parentDiv.find('ul', {'class':'results-list with-flags'})
-    itemContainer = contentContainer('li')
     try:
         endChild = parentDiv.find('a', {'class':'next_page'})
         EndNode = endChild.find_previous_sibling()
         End = int(EndNode.text)
     except:
         print('scrape END value GET failed.  Check HTML for cause.')
+        End = 69
+    return End
 
 #Write dataframe from openLeis JSON and push to MongoDB
 def write_df_to_mongoDB(
@@ -71,6 +72,7 @@ def write_df_to_mongoDB(
     server = 'localhost',\
     mongodb_port = 27017,\
     chunk_size = 30):
+    # End = getEnd().End):
     col.create_index("_id")
     # To write
 
@@ -83,49 +85,46 @@ def write_df_to_mongoDB(
         # print(item)
         try:
             db.leisJSON.update_one({'_id':item['lei']}, {'$set':item},upsert=True)
-            # print(item)
-            # print("Insert_One Success")
         except pymongo.errors.ConnectionFailure as e: 
-            # print("Insert_One Failed")
             print("Insert_OneError:",e)
-       
-    print('Script Done')
-    return
-
+    
 
 #==========================================================>
 
-#This should be the base url you wanted to access.
-baseurl = 'http://openleis.com'
-path = '/legal_entities.csv'
-pathJSON = '/legal_entities.json'
-# page 1 sample
-# http://openleis.com/legal_entities.json
-# http://openleis.com/legal_entities/search/page/1.json
-# page 2 sample
-# http://openleis.com/legal_entities/search/page/2.json
-# r = requests.get(baseurl + path, auth=(username,password))
 
 #=====================variables==========================>
 Finished = False
 countItem = 1
+page = 1
+
 #=============================================================>
 # Get this work started!!!!
-while Finished == False:
-    # Start of getEnd function
-    while End == 0:
-        try:
-            getEnd(page)
-        except Exception as e:
-            print("getEnd function failed")
-
+# Start of getEnd function
+while End == 0:
     try:
+        # End get value and declare as variable
+        # End = getEnd(page)
+        End = 30
+        # print("End: ",End)
+
+    except Exception as e:
+        print("getEnd function failed")
+        End = 1
+# begin 
+while Finished == False:
+    try:
+        #This should be the base url you wanted to access.
+        baseurl = 'http://openleis.com'
+        # path = '/legal_entities.csv'
+        # sample = http://openleis.com/legal_entities/search/page/1.json
+        pathJSON = '/legal_entities/search/page/' + str(page) + '.json'
         urlData = urllib.request.urlopen(baseurl + pathJSON)
         data = urlData.read()
         extractedContent = json.loads(data)
+        print(extractedContent)
         # encoding = urlData.info().get_content_charset('utf-8')
         # decoding = data.decode(encoding)
-        Finished = True
+        # Finished = True
 
         # df = pd.DataFrame([extractedContent])
         # def dict_to_df
@@ -134,17 +133,30 @@ while Finished == False:
         print(str(e))
     
     # print(df.head())
-my_df = pd.DataFrame.from_dict(json_normalize(extractedContent),orient='columns')
-my_df.rename(columns=lambda x: x.replace('.', ''), inplace=True)
+    my_df = pd.DataFrame.from_dict(json_normalize(extractedContent),orient='columns')
+    my_df.rename(columns=lambda x: x.replace('.', ''), inplace=True)
+
+    # Function call and iterate up
+    write_df_to_mongoDB(my_df)
+    lastPage = page
+    print("last page completed:",lastPage)
+    # page End check to see if we should continue
+    if page > End:
+        print('Data.gov scrape complete.')
+        Finished = True
+        exit
+    else: 
+        page = page + 1
+        Finished = False
+
 
 # Call dataFrame to MongoDB formula
 # WORKFLOW
 
-
-write_df_to_mongoDB(my_df)
-
-print("End: ",End)
 print("Script Finished Running")
+print("Last ditch fail catch")
+Finished = True
+
 
 
 
